ea4ffe7f129cb330b633f7ede32b6ccb4c1186dc
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-21 10:25:41 -0700

    add installer

---
+++ install.sh
@@ -0,0 +1,7 @@
+SRCDIR=$PWD
+cd ~
+ln -s $SRCDIR/lib lib
+echo 'source ~/lib/profile' >> ~/.bashrc
+ln -s $SRCDIR/.emacs.d .emacs.d
+ln -s $SRCDIR/.tmux.conf .tmux.conf
+ln -s $SRCDIR/.gitconfig .gitconfig
