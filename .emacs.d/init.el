e4ae3c3c9cabeba82011c625418e6840e89a9ee7
Aron Fyodor Asor <aronasorman@gmail.com>
2014-03-13 21:51:47 -0700

    use tomorrow night as default theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,9 +113,13 @@
 (use-package color-theme
   :ensure t)
 
-(use-package sunburn
-  :load-path "~/.emacs.d/"
-  :config (color-theme-sunburn))
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :config (color-theme-sanityinc-tomorrow-night))
+
+;; (use-package sunburn
+;;   :load-path "~/.emacs.d/"
+;;   :config (color-theme-sunburn))
 
 (use-package shm
   :load-path "~/.emacs.d/structured-haskell-mode/elisp"
@@ -485,7 +489,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" default)))
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" default)))
+ '(fci-rule-color "#efefef")
  '(midnight-mode t nil (midnight))
  '(org-agenda-files (quote ("~/notes/todo/personal.org" "~/notes/todo/habits.org" "~/notes/todo/fle.org")))
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
@@ -495,7 +503,10 @@
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 587)
- '(web-mode-code-indent-offset 4))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#c82829") (40 . "#f5871f") (60 . "#eab700") (80 . "#718c00") (100 . "#3e999f") (120 . "#4271ae") (140 . "#8959a8") (160 . "#c82829") (180 . "#f5871f") (200 . "#eab700") (220 . "#718c00") (240 . "#3e999f") (260 . "#4271ae") (280 . "#8959a8") (300 . "#c82829") (320 . "#f5871f") (340 . "#eab700") (360 . "#718c00"))))
+ '(vc-annotate-very-old-color nil)
+ '(web-mode-code-indent-offset 4 t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
