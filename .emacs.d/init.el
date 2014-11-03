f44207154e0cd0d17415dd5f114fd5a7e9b1451f
Aron Fyodor Asor <aronasorman@gmail.com>
2014-11-02 19:05:56 -0800

    show paren range of 3 in evil

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -822,6 +822,7 @@ screen."
  '(ansi-color-names-vector (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" default)))
+ '(evil-show-paren-range 3)
  '(fci-rule-color "#efefef")
  '(midnight-mode t nil (midnight))
  '(org-agenda-files (quote ("~/notes/todo/fle.org")))
